95411b3d6615c3e458984f5b6349a8a8741330aa
Satish Ayyaswami <satish@tsiconsulting.in>
2022-10-02 06:41:33 +0530

    initial changes

---
+++ src/in/tsiconsulting/accelerator/solutions/finance/loan/los/TSI.java
@@ -0,0 +1,41 @@
+package in.tsiconsulting.accelerator.solutions.finance.loan.los;
+
+import in.tsiconsulting.accelerator.system.core.REST;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TSI implements REST {
+
+    private static final String API_PROVIDER = "tsi";
+
+    private static final String API_NAME = "los";
+
+    private static final String METHOD = "_method";
+    private static final String DATA = "_data";
+
+    @Override
+    public void get(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+
+    @Override
+    public void post(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+
+    @Override
+    public void delete(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+
+    @Override
+    public void put(HttpServletRequest req, HttpServletResponse res) {
+
+    }
+
+    @Override
+    public void validate(String method, HttpServletRequest req, HttpServletResponse res) {
+
+    }
+}
