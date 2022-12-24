6493dc015ba26a213e07bae61afbb59659bdaa7a
TSI Consulting LLP <satish@tsiconsulting.in>
2022-12-24 17:40:19 +0530

    Cleanup

---
+++ tsi-business-server.bat
@@ -0,0 +1,3 @@
+title TSI Business Server
+set JAVA_HOME=C:\software\jdk-17
+C:\software\apache-ant-1.9.16\bin\ant run
--- tsi.bat
+++
@@ -1,3 +0,0 @@
-title TSI Business Service
-set JAVA_HOME=C:\software\jdk-17
-C:\software\apache-ant-1.9.16\bin\ant run
