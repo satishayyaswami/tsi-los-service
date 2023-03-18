ad3f3caf1fd2d80889556149ee6919bc2cc43165
TSI Consulting LLP <satish@tsiconsulting.in>
2023-03-18 21:15:57 +0530

    Added EventListener and EventProcessor

--- src/in/tsiconsulting/accelerator/business/events/EventListener.java
+++ src/in/tsiconsulting/accelerator/business/events/EventListener.java
@@ -1,4 +1,20 @@
 package in.tsiconsulting.accelerator.business.events;
 
-public class EventListener {
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class EventListener implements ServletContextListener {
+
+    EventProcessor ep = null;
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event) {
+        ep.stop();
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        ep = new EventProcessor();
+        ep.start();
+    }
 }
--- src/in/tsiconsulting/accelerator/business/events/EventProcessor.java
+++ src/in/tsiconsulting/accelerator/business/events/EventProcessor.java
@@ -1,4 +1,57 @@
 package in.tsiconsulting.accelerator.business.events;
 
 public class EventProcessor {
+
+    public final static long DAFAULT_MAX_TIME_INTERVAL = 500;
+    public final static long DAFAULT_MAX_RECORD_LIMIT = 10;
+    private static String LAST_MASTER_SYNC_DATE = null;
+
+    LoanApplicationProcessor lap = null;
+    CollectionProcessor cp = null;
+
+    private static class LoanApplicationProcessor extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+
+                    Thread.sleep(DAFAULT_MAX_TIME_INTERVAL);
+                } catch(Exception e) {
+                } finally {
+                }
+            } while(true);
+        }
+    };
+
+    private static class CollectionProcessor extends Thread {
+        @Override
+        public void run() {
+            do {
+                try {
+
+                    Thread.sleep(DAFAULT_MAX_TIME_INTERVAL);
+                } catch(Exception e) {
+                } finally {
+                }
+            } while(true);
+        }
+    };
+
+    public void start() {
+        lap = new LoanApplicationProcessor();
+        cp = new CollectionProcessor();
+        lap.start();
+        cp.start();
+        System.out.println("Loan Application Processor Started");
+        System.out.println("Collection Processor Started");
+    }
+
+    public void stop() {
+        lap.interrupt();
+        cp.interrupt();
+    }
+
+    public static void main(String[] args){
+        new EventProcessor().start();
+    }
 }
