be6179ff136adeb38c45975cc8a8736798d55695
Divyush Raj <Divyush Raj@Divyush-Raj>
2023-03-26 17:33:45 +0530

    initial push to workbranch, kyc validations added

--- tsi-backend-service.bat
+++ tsi-backend-service.bat
@@ -1,3 +1,3 @@
 title TSI Backend Service
-set JAVA_HOME=C:\software\jdk-17
-C:\software\apache-ant-1.9.16\bin\ant run
+set JAVA_HOME=C:\Program Files\Java\jdk-17.0.5\bin
+D:\softwares\apache-ant-1.10.12-bin\apache-ant-1.10.12\bin\ant run
