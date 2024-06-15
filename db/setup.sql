a05c8f3957bab30c629289291aafdbd16b2aaa15
TSI Consulting LLP <satish@tsiconsulting.in>
2024-06-16 04:55:08 +0530

    Readme

--- db/setup.sql
+++ db/setup.sql
@@ -1,5 +1,5 @@
-# Create TSI Backend Service
-create database _tsi_backend_service;
+# Create TSI LOS Service
+create database _tsi_los_service;
 CREATE USER demo WITH PASSWORD 'demo@123';
-GRANT CONNECT ON DATABASE _tsi_backend_service TO demo;
-GRANT ALL PRIVILEGES ON DATABASE _tsi_backend_service TO demo;
+GRANT CONNECT ON DATABASE _tsi_los_service TO demo;
+GRANT ALL PRIVILEGES ON DATABASE _tsi_los_service TO demo;
